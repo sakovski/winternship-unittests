6bde53ef048b20dba6a8e8c87efb8428e52ccdb3
Przemysław Jankowski <p.jankowski.14@wp.pl>
2022-01-12 14:17:28 +0100

    4 VoteService class tests

--- src/test/java/poll/service/VoteServiceTest.java
+++ src/test/java/poll/service/VoteServiceTest.java
@@ -29,50 +29,74 @@ public class VoteServiceTest {
 
 
     @Test
-    public void should_ReturnVote_when_GivenVoteId() {
+    public void should_ReturnVote_when_GivenVoteIdAndVoteFound() {
         //given
         Vote expectedVote = new Vote(3l, "Ivan Zaitsev", LocalDateTime.of(2021, 1, 2, 9, 30), Answer.YES);
-        //when
         when(voteRepository.findById(3l)).thenReturn(Optional.of(expectedVote));
+        //when
         //then
-        assertEquals(voteService.getVoteById(3l), expectedVote);
+        assertEquals(expectedVote,voteService.getVoteById(3l));
     }
 
     @Test
     public void should_ThrowVoteFoundException_when_NotFoundVoteId() {
         //given
         long id = 100l;
-        //when
         when(voteRepository.findById(id)).thenReturn(Optional.empty());
+        //when
         //then
         assertThrows(VoteNotFoundException.class, () -> voteService.getVoteById(id));
     }
 
 
     @Test
-    public void should_ReturnEmptyVote_when_NotFoundVoteId(){
+    public void should_ReturnEmptyVote_when_NotFoundVoteId() {
         //given
         long id = 100l;
-        Vote emptyVote = new Vote(id, null,null,null);
-        //when
+        Vote emptyVote = new Vote(id, null, null, null);
         when(voteRepository.findById(id)).thenReturn(Optional.of(emptyVote));
+        //when
         //then
-        assertSame(emptyVote,voteService.getOrCreateVote(id));
+        assertSame(emptyVote, voteService.getOrCreateVote(id));
     }
 
     @Test
-    public void should_ReturnListVoteWithYesAnswerByName_when_GivenName(){
+    public void should_ReturnListVoteWithYesAnswerByName_when_GivenName() {
         //given
-        List<Vote> voteList = Arrays.asList(
-                     new Vote(1l, "David Smith", LocalDateTime.of(2021, 1, 1, 14, 30), Answer.YES),
-                     new Vote(2l, "David Smith", LocalDateTime.of(2021, 1, 1, 15, 30), Answer.YES));
+        List<Vote> expectedVoteList = Arrays.asList(
+                new Vote(1l, "David Smith", LocalDateTime.of(2021, 1, 1, 14, 30), Answer.YES),
+                new Vote(2l, "David Smith", LocalDateTime.of(2021, 1, 1, 15, 30), Answer.YES),
+                new Vote(2l, "David Smith", LocalDateTime.of(2021, 1, 1, 15, 30), Answer.YES));
+        when(voteRepository.findByName("David Smith")).thenReturn(expectedVoteList);
         //when
-        when(voteRepository.findByName("David Smith")).thenReturn(voteList);
         //then
-        assertEquals(voteList,voteService.getAllYesVotesByName("David Smith"));
+        assertEquals(expectedVoteList, voteService.getAllYesVotesByName("David Smith"));
     }
 
+    @Test
+    public void should_returnNumberOfVotesByAnswer_when_GivenAnswer(){
+        //given
+        int expectedNumber = 4;
+        List<Vote> votesListWithYesAnswer = Arrays.asList(
+                new Vote(5l, "Uros Kovacevic", LocalDateTime.of(2021, 1, 2, 10, 0), Answer.YES),
+                new Vote(6l, "Marko Podrascanin", LocalDateTime.of(2021, 1, 2, 14, 30), Answer.YES),
+                new Vote(7l, "Swan Ngapeth", LocalDateTime.of(2021, 1, 3, 8, 0), Answer.YES),
+                new Vote(8l, "Kamil Semeniuk", LocalDateTime.of(2021, 1, 3, 14, 0), Answer.YES));
+        when(voteRepository.findByAnswer(Answer.YES)).thenReturn(votesListWithYesAnswer);
+        //then
+        assertEquals(expectedNumber,voteService.countAllVotesOnAnswer(Answer.YES));
+    }
 
+    @Test
+    public void should_returnTrue_when_GivenAnswerWonTheVote(){
+        boolean expectedReturn = true;
+        List<Vote> votesListWithYesAnswer = Arrays.asList(
+                new Vote(5l, "Uros Kovacevic", LocalDateTime.of(2021, 1, 2, 10, 0), Answer.YES),
+                new Vote(6l, "Marko Podrascanin", LocalDateTime.of(2021, 1, 2, 14, 30), Answer.YES),
+                new Vote(7l, "Swan Ngapeth", LocalDateTime.of(2021, 1, 3, 8, 0), Answer.NO),
+                new Vote(8l, "Kamil Semeniuk", LocalDateTime.of(2021, 1, 3, 14, 0), Answer.YES));
+//    when(voteRepository.findAll()).thenReturn()
 
+    }
 
 }
